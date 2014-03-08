add3029c41a87fe731e5a38b7af872332b354373
soasme <soasme@gmail.com>
2014-03-08 20:25:10 +0800

    Define JIMCenter.

---
+++ JIMCenter.h
@@ -0,0 +1,13 @@
+//
+//  JIMCenter.h
+//  jabby
+//
+//  Created by 林 炬 on 14-3-8.
+//  Copyright (c) 2014年 soasme. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JIMCenter : NSObject
+
+@end
---
+++ JIMCenter.m
@@ -0,0 +1,13 @@
+//
+//  JIMCenter.m
+//  jabby
+//
+//  Created by 林 炬 on 14-3-8.
+//  Copyright (c) 2014年 soasme. All rights reserved.
+//
+
+#import "JIMCenter.h"
+
+@implementation JIMCenter
+
+@end
