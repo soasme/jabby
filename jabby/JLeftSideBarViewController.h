ec245e21997c2cb2ac1a55ad5eaa654b30566662
soasme <soasme@gmail.com>
2014-03-22 10:37:16 +0800

    Add LeftSideBarviewController.

---
+++ jabby/JLeftSideBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  JLeftSideBarViewController.h
+//  jabby
+//
+//  Created by 林 炬 on 14-3-22.
+//  Copyright (c) 2014年 soasme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JLeftSideBarViewController : UITableViewController
+
+@end
